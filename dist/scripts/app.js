fc844299b6f57f2566f1a095fe6e54ce7d533f66
Bharani <bharani91@gmail.com>
2015-11-19 17:49:33 +0530

    Removed default annotations

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -10,59 +10,10 @@ jQuery(document).ready(function($) {
         "exam"
     ];
 
-    var defaultAnnotations =  [
-        {
-            range: {
-                startOffset: 28,
-                endOffset: 77,
-                startContainerXPath: "id(\"book\")/p[5]",
-                endContainerXPath: "id(\"book\")/p[5]",
-                parentContainerXPath: "id(\"book\")/p[5]"
-            },
-            id: "annotation-1435820385354",
-            selectedText: "Boojho recalled the consequences of deforestation",
-            color: "pink",
-            note: "This is my note"
-
-        },
-
-
-        {
-            range: {
-                startOffset: 41,
-                endOffset: 379,
-                startContainerXPath: "id(\"book\")/p[7]",
-                endContainerXPath: "id(\"book\")/p[7]",
-                parentContainerXPath: "id(\"book\")/p[7]"
-            },
-            selectedText: "increased chances of natural calamities such as floods and droughts. Recall that plants need carbon dioxide for photosynthesis. Fewer trees would mean that less carbon dioxide will be used up resulting in its increased amount in the atmosphere. This will lead to global warming as carbon dioxide traps the heat rays reflected by the earth",
-            color: "green",
-            note: "This is another note"
-        },
-
-        {
-            range: {
-                startOffset: 274,
-                endOffset: 497,
-                startContainerXPath: "id(\"book\")/p[7]",
-                endContainerXPath: "id(\"book\")/p[7]",
-                parentContainerXPath: "id(\"book\")/p[7]"
-            },
-            id: "annotation-1435824292078",
-            selectedText: "atmosphere. This will lead to global warming as carbon dioxide traps the heat rays reflected by the earth. The increase in temperature on the earth disturbs the water cycle and may reduce rainfall. This could cause droughts",
-            color: "yellow"
-        }
-    ];
 
     var annotations = (function() {
         if(window.localStorage) {
-            var annotations = JSON.parse(localStorage.getItem("annotations"));
-
-            if( !annotations || annotations.length == 0 ) {
-                window.localStorage.setItem("annotations", JSON.stringify(defaultAnnotations));
-                return defaultAnnotations;
-            }
-            return annotations;
+            return JSON.parse(localStorage.getItem("annotations"));
         } else {
             return [];
         }
